02822f7cabb452d0d6db4c6062210dd37b482133
Djessy Coiffé <116000492+Djessy-lab@users.noreply.github.com>
2025-01-06 17:37:22 +0100

    Ajout du fichier TheLabOne/components/Landing/HowItsWorks.vue

---
+++ TheLabOne/components/Landing/HowItsWorks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="imgArg" class="mx-auto rounded-3xl" :style="{ width: imgArgWidth + '%', height: imgArgHeight + 'rem' }">
+    <img :class="imageClass" :src="imgArg">
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HowItsWorks',
+  props: {
+    imgArg: { type: String, default: "" },
+    imgArgShadow: { type: Boolean, default: false },
+    imgArgRounded: { type: Boolean, default: false },
+    imgArgPosition: { type: String, default: 'center' },
+    imgArgFit: { type: String, default: 'contain' },
+    imgArgWidth: { type: String, default: '50' },
+    imgArgHeight: { type: String, default: '50' },
+  },
+  computed: {
+    imageClass() {
+      let classes = `object-${this.imgArgFit} object-${this.imgArgPosition} w-full h-full`;
+      if (this.imgArgShadow) classes += ' shadow-2xl';
+      if (this.imgArgRounded) classes += ' rounded-xl';
+      return classes;
+    },
+  }
+}
+</script>
